05622c5f516ecca3d6c79a525cb60c6e85453f3a
Nikita Lukjanov <nlukjanov@gmail.com>
2019-07-17 18:24:02 +0300

    added signin form component

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+
+import FormInput from "../form-input/form-input.component";
+
+import "./sign-in.styles.scss";
+
+class SignIn extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    this.setState({
+      email: "mike@gmail.com",
+      password: "123"
+    });
+  };
+
+  handleChange = event => {
+    const { value, name } = event.target;
+
+    this.setState({ [name]: value });
+  };
+
+  render() {
+    return (
+      <div className="sign-in">
+        <h2>I already have account</h2>
+        <span>Sign in with your email and password</span>
+
+        <form onSubmit={this.handleSubmit}>
+          <FormInput
+            name="email"
+            type="email"
+            handleChange={this.handleChange}
+            value={this.state.email}
+            label="email"
+            required
+          />
+          <FormInput
+            name="password"
+            type="password"
+            handleChange={this.handleChange}
+            value={this.state.password}
+            label="password"
+            required
+          />
+          <input type="submit" value="Submit Form" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignIn;
---
+++ src/pages/signinpage/signinpage.component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import SignIn from "../../components/sign-in/sign-in.component";
+
+import "./signinpage.styles.scss";
+
+const SigninPage = () => {
+  return (
+    <div className="signin">
+      <SignIn />
+    </div>
+  );
+};
+
+export default SigninPage;
