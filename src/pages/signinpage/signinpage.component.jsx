1c775f2f4d289bf8c9a90b7bdfe863590ffc3aca
Nikita Lukjanov <nlukjanov@gmail.com>
2019-07-22 17:36:39 +0300

    added sign-up with email component via firebase

--- src/pages/signinpage/signinpage.component.jsx
+++ src/pages/signinpage/signinpage.component.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 
 import SignIn from "../../components/sign-in/sign-in.component";
+import SignUp from "../../components/sign-up/sign-up.component";
 
 import "./signinpage.styles.scss";
 
-const SigninPage = () => {
-  return (
-    <div className="signin">
-      <SignIn />
-    </div>
-  );
-};
+const SigninPage = () => (
+  <div className="sign-in-and-sign-up">
+    <SignIn />
+    <SignUp />
+  </div>
+);
 
 export default SigninPage;
