925cf18ef3046dc4f181631bc3f62b762ac7e138
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-06-19 12:07:38 +0100

    git ignore firebase config

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,16 +1,7 @@
-import firebase from "firebase/app";
-import "firebase/firestore";
-import "firebase/auth";
-
-const config = {
-  apiKey: "AIzaSyA5o8IgB03B5mk4n8uR9EoeQ9Y7Y4i1VcM",
-  authDomain: "ecommerce-7fec4.firebaseapp.com",
-  databaseURL: "https://ecommerce-7fec4.firebaseio.com",
-  projectId: "ecommerce-7fec4",
-  storageBucket: "",
-  messagingSenderId: "734886683249",
-  appId: "1:734886683249:web:21eabe3f4a5777bb"
-};
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+import { config } from './firebase.config';
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
@@ -30,7 +21,7 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
         ...additionalData
       });
     } catch (error) {
-      console.log("Error creating user", error.message);
+      console.log('Error creating user', error.message);
     }
   }
 
@@ -43,7 +34,7 @@ export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: "select_account" });
+provider.setCustomParameters({ prompt: 'select_account' });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
