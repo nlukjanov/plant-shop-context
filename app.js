c9762b3e34dfa9e99c48ab08625e361b36e8c185
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-07-03 11:15:31 +0100

    add stripe transaction handling on backend

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const path = require('path');
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 const app = express();
 
@@ -13,9 +16,24 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('*', function (req, res) {
-    console.log('sending')
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
+
+app.post('/payment', (req, res) => {
+  const body = {
+    source: req.body.token.id,
+    amount: req.body.amount,
+    currency: 'usd'
+  };
+
+  stripe.charges.create(body, (stripeErr, stripeRes) => {
+    if (stripeErr) {
+      res.status(500).send({ error: stripeErr });
+    } else {
+      res.status(200).send({ success: stripeRes });
+    }
+  });
+});
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 const app = require('./app');
 const port = process.env.PORT || 5000;
+
 
 app.listen(port, (error) => {
   if (error) throw error;
