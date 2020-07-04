427b6b85948f2d02859d4dbf3c2c23c600335d19
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-07-04 12:07:39 +0100

    refactored some components into styled components

--- client/src/components/cart-item/cart-item.component.jsx
+++ client/src/components/cart-item/cart-item.component.jsx
@@ -1,17 +1,23 @@
-import React from "react";
-import './cart-item.styles.scss';
+import React from 'react';
+
+import {
+  CartItemContainer,
+  ImageContainer,
+  ItemDetailsContainer,
+  ItemName
+} from './cart-item.styles';
 
 const CartItem = ({ item: { imageUrl, price, name, quantity } }) => {
   return (
-    <div className="cart-item">
-      <img src={imageUrl} alt="item" />
-      <div className="item-details">
-        <span className="name">{name}</span>
-        <span className="price">
+    <CartItemContainer>
+      <ImageContainer src={imageUrl} alt='item' />
+      <ItemDetailsContainer>
+        <ItemName>{name}</ItemName>
+        <span className='price'>
           {quantity} x ${price}
         </span>
-      </div>
-    </div>
+      </ItemDetailsContainer>
+    </CartItemContainer>
   );
 };
 
