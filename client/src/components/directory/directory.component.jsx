2faa1be8d2254f0229d2a347a6c7dcde0f56e8bb
nlukjanov <16008663+nlukjanov@users.noreply.github.com>
2020-07-16 17:23:45 +0100

    refactor directory to use context

--- client/src/components/directory/directory.component.jsx
+++ client/src/components/directory/directory.component.jsx
@@ -1,23 +1,20 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
-
-import { selectDirectorySections } from '../../redux/directory/directory.selectors';
+import React, { useContext } from 'react';
 
 import MenuItem from '../menu-item/menu-item.component';
 
+import DirectoryContext from '../../context/directory/directory.context';
+
 import { DirectoryMenuContainer } from './directory.styles';
 
-const Directory = ({ sections }) => (
-  <DirectoryMenuContainer>
-    {sections.map(({ id, ...otherSectionProps }) => (
-      <MenuItem key={id} {...otherSectionProps} />
-    ))}
-  </DirectoryMenuContainer>
-);
+const Directory = () => {
+  const sections = useContext(DirectoryContext);
+  return (
+    <DirectoryMenuContainer>
+      {sections.sections.map(({ id, ...otherSectionProps }) => (
+        <MenuItem key={id} {...otherSectionProps} />
+      ))}
+    </DirectoryMenuContainer>
+  );
+};
 
-const mapStateToProps = createStructuredSelector({
-  sections: selectDirectorySections
-});
-
-export default connect(mapStateToProps)(Directory);
+export default Directory;
